aa8c2bfa886cee07e9672bbbe0241855adca9e59
Ce <109117220+AprilianoPeramu@users.noreply.github.com>
2023-01-19 21:41:07 -0800

    edit button in nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import {BiPlus} from "react-icons/bi";
 
 const Navbar = () => {
   return (
     <div>
         <div className='d-flex align-items-center mt-1'>
         <h5 className='ms-4 mb-2'>Product Roadmap</h5>
-        <button className='butn ms-2 justify-content-start'> + Add New Group</button>
+        <button className='butn ms-2 justify-content-start'><BiPlus className='nav-icon'/> Add New Group</button>
         </div>
         <hr className='navline'/>
     </div>
